b6d35cf99ba4a1774e7f97397c661156fc5cbfd2
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2018-07-24 22:34:29 -0300

    heroes list ocntainer

--- src/app/heroes-module/components/lists/heroes/heroes.component.ts
+++ src/app/heroes-module/components/lists/heroes/heroes.component.ts
@@ -1,54 +1,15 @@
-import { Observable } from 'rxjs/Observable';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 import { Hero } from '../../../../hero';
-import { HeroService } from '../../../../hero.service';
-import { Store } from '@ngrx/store';
-
-import { AppState } from './../../../../state-manegment/app.reducers';
-import { LoadHeroesRequesAction } from '../../../../state-manegment/stores';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent implements OnInit {
+export class HeroesComponent {
 
-  heroes$: Observable<Hero[]>;
+  @Input() heroes: Hero[];
 
-  heroes: Hero[];
-
-  constructor(
-    private heroService: HeroService,
-    private store: Store<AppState>
-  ) {
-    this.heroes$ = store.select(state => state.heroes.heroes);
-  }
-
-
-  ngOnInit() {
-    this.getHeroes();
-    this.store.dispatch(new LoadHeroesRequesAction());
-  }
-
-  getHeroes(): void {
-    this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes);
-  }
-
-  add(name: string): void {
-    this.heroService.addHero({ name } as Hero)
-      .subscribe(hero => {
-        console.log(hero);
-
-        this.heroes.push(hero);
-      });
-  }
-
-  delete(hero: Hero): void {
-    this.heroes = this.heroes.filter(h => h !== hero);
-    this.heroService.deleteHero(hero).subscribe();
-  }
 
 }
--- src/app/heroes-module/pages/heroes-page/heroes-page.component.ts
+++ src/app/heroes-module/pages/heroes-page/heroes-page.component.ts
@@ -1,4 +1,9 @@
+import { AppState } from './../../../state-manegment/app.reducers';
+import { Hero } from './../../../hero';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Store } from '@ngrx/store';
+import { LoadHeroesRequesAction } from '../../../state-manegment/stores';
 
 @Component({
   selector: 'app-heroes-page',
@@ -7,9 +12,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesPageComponent implements OnInit {
 
-  constructor() { }
+  heroes$: Observable<Hero[]>;
 
-  ngOnInit() {
+  heroes: Hero[];
+
+  constructor(
+    private store: Store<AppState>
+  ) {
+    this.heroes$ = store.select(state => state.heroes.heroes);
   }
 
+
+  ngOnInit() {
+    this.store.dispatch(new LoadHeroesRequesAction());
+  }
+
+
+  onAddHero(name: string): void {
+    /*     this.heroService.addHero({ name } as Hero)
+          .subscribe(hero => {
+            console.log(hero);
+
+            this.heroes.push(hero);
+          }); */
+  }
+
+  delete(hero: Hero): void {
+    // this.heroes = this.heroes.filter(h => h !== hero);
+    //  this.heroService.deleteHero(hero).subscribe();
+  }
 }
